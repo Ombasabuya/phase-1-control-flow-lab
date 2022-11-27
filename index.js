0c6c4e394fff506a6bd8ba080621f98ff00e625d
ombasaabuya <ombasabuya@gmail.com>
2022-11-27 13:32:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+
+  let result;
+  if(distance < 400 || distance === 400){
+    result = 'This one is on me!';
+  }
+  else if(distance > 2000 && distance < 2500){
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else if(distance > 2500){
+    result = 'No can do.'
+  }
+  return result;
+}
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501)
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  let result;
+
+  city === 'NYC'? result ="Ok, sounds good." : result = "No go."
+
+  return result;
 }
 
-function ternaryCheckCity(){
+ternaryCheckCity('NYC');
+ternaryCheckCity('Pittsburgh');
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let result;
+  switch(tip){
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+
+    case 'not as generous':
+      result = 'Thank you.';
+      break;
+
+    default:
+      result = 'Bye.';
+  }
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+switchOnCharmFromTip('generous');
+switchOnCharmFromTip('not as generous');
+switchOnCharmFromTip('thanks for everthing');
